d1ea48b91e17f03c66da5befd413581db587662b
Mairon Vilela <maironvilela@hotmail.com>
2022-08-11 12:42:41 -0300

    refactor: menu icon in the Header Component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,24 +1,43 @@
 import { useMenuToggle } from '../../hooks/menu-toggle';
-import { MenuToggle } from './menu-toggle';
+import { MenuIconSVG } from '../../svg/menu-icon';
 import styles from './styles.module.scss';
 
 export function Header() {
-  const { setToggleMenu } = useMenuToggle();
+  const { setToggleMenu, isShowMenu } = useMenuToggle();
 
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <button onClick={() => setToggleMenu()}>
-          <img src="../src/assets/images/menu-icon.svg" alt="icone do menu" />;
+        <button
+          onClick={() => setToggleMenu()}
+          className={`${isShowMenu ? styles['show-menu'] : ''}`}
+        >
+          <MenuIconSVG />
         </button>
+        {isShowMenu ? (
+          <nav>
+            <ul>
+              <li>
+                <a href="#list"> Lista</a>
+              </li>
+              <li>
+                <a href="#about_me"> Sobre Mim</a>
+              </li>
+              <li>
+                <a href="#register"> Cadastro</a>
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          ''
+        )}
+
         <img
           src="../src/assets/images/logo-in8-dev.svg"
           alt="Logo"
           className={styles.logo}
         />
       </div>
-
-      <MenuToggle />
     </header>
   );
 }
